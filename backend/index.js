523294c06e47ac8fca2f327582a87144f58171a5
luiz <lduducamposs@gmail.com>
2026-07-13 18:42:41 -0300

    primeira rota e porta

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req , res) => {
+    res.send("teste");
+})
+
+
+
+app.listen(port, () => {
+    console.log("servidor rodando na porta "+ port)
+});

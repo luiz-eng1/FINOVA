88afb14247608912a2a7d7243736da0ecac84dda
luiz <lduducamposs@gmail.com>
2026-07-14 18:29:43 -0300

    conexao de teste para o pool.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import "dotenv/config";
-
+import pool from "./src/model/pool.js"
 
 
 
@@ -8,14 +8,28 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json()); // middleware sempre antes das routes
 
 app.get("/", (req , res) => {
     res.send("teste");
 })
 
 
+async function testarConexao(){
+    try{
+        const resultado = await pool.query("SELECT NOW()"); //await so pode ser usado dentro de uma função asynmc
+        console.log(resultado.rows); // .rows retorna apenas os dados
+    }catch(err){
+        console.error("Erro ao conectar: ", err);
+    }
+}
+
+testarConexao();
+
 
 app.listen(port, () => {
     console.log("servidor rodando na porta "+ port)
 });
+
+
+
--- backend/src/model/pool.js
+++ backend/src/model/pool.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD
 });
 
 export default pool;
+

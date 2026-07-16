ce386f9d51b140a56f5434cc24de636c256127e7
luiz <lduducamposs@gmail.com>
2026-07-16 18:29:12 -0300

    function controller login

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ const app = express();
 
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log("REQUISIÇÃO CHEGOU:", req.method, req.url);
+  next();
+});
+
 app.use(express.json()); // middleware sempre antes das routes
 
 
--- backend/src/controller/usuario.controller.js
+++ backend/src/controller/usuario.controller.js
@@ -1,20 +1,21 @@
 import { buscarPorEmail, criarUsuario } from "../model/usuario.model.js";
 import bcrypt from "bcrypt"
-
+import jwt from "jsonwebtoken"
 
 
 
 export async function cadastrar(req, res){
-    
+
 
 try{
     const nome = req.body.nome;
     const email = req.body.email;
     const senha = req.body.senha;
 
+    
+
     // verifica se todos os campos foram enviados
     if(!nome || !email || !senha ){
-    
         return res.status(400).json({
             erro: "Nome, email e senha são obrigatórios."
         })
@@ -27,7 +28,12 @@ try{
         })
     }
 
+    
+
     const buscaEmail = await buscarPorEmail(email);
+
+    
+
     if(buscaEmail){
         return res.status(409).json({
             erro: "O email já possui uma conta existente."
@@ -36,18 +42,86 @@ try{
 
     const senhaHash = await bcrypt.hash(senha, 10); // hash da senha
 
+    
+
     const usuario = await criarUsuario(nome, email, senhaHash);
+
+    
 
     return res.status(201).json(usuario);
 }catch(erro){
-
-
     console.log(erro);
     return res.status(500).json({
         erro: "Não foi possivel criar um usuario."
     })
 }
 
+}
 
+
+
+
+export async function login(req, res){
+ 
+
+    if(!email || !senha){
+        return res.status(400).json({
+            erro: "Email e senha são obrigatórios"
+        })
+    }
+
+
+try{
+
+    //recebe email e senha
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    const usuario = await buscarPorEmail(email);
+
+    if(!usuario){
+        return res.status(401).json({
+            erro: "Email ou senha inválidos"
+        })
+    }
+
+   
+    const senhaConfere = await bcrypt.compare(senha, usuario,senha);
+
+    //verifica se a senha está correta
+    if(!senhaConfere){
+        return res.status(401).json({
+            erro: "Email o senha inválidos"
+        })
+    }
+
+    // cria o jwtoken
+    const token = jwt.sign(
+        {id: usuario.id},
+        process.env.JWT_SECRET,
+        {expiresIn: "1d"}
+    )
+
+
+    return res.status(200).json({
+        token: token,
+        usuario: {
+            id: usuario.id,
+            nome: usuario.nome,
+            email: usuario.email
+        }
+    })
+
+
+}catch(erro){
+
+    console.error(erro);
+
+    return res.status(500).json({
+        erro: "Erro. Tente novamente mais tarde."
+    })
+
+    
+}
 
 }

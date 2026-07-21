3184ba7b1a0ce07a389d882d96339f6456d36577
luiz <lduducamposs@gmail.com>
2026-07-21 19:08:22 -0300

    pagina de dashboard com função de carregar (com hook useEffect

--- frontend/vite-project/src/pages/Dashboard.jsx
+++ frontend/vite-project/src/pages/Dashboard.jsx
@@ -1,8 +1,50 @@
+import {useState, useEffect} from "react"
+
+
 function Dashboard(){
+
+    //carregando os dados da API com os 3 estados
+    const [dados, setDados] = useState([]);
+    const [carregando, setCarregando] = useState(true)
+    const [erro, setErro] = useState("")
+
+    useEffect(() => {
+        async function carregar (){
+
+            try{
+                const token = localStorage.getItem("token");
+
+                const resposta = await fetch("http://localhost:3000/transacoes", {
+                headers: { Authorization: "Bearer " + token }
+                });
+
+                const resultado = await resposta.json();
+                if(resposta.ok){
+                    setDados(resultado);
+                }else{
+                    setErro("Não foi possivel carregar as transações");
+                }
+                setCarregando(false);
+            }catch(erro){
+                console.error(erro);
+                setCarregando(false);
+                setErro("Não foi possivel carregar as transações");
+            }
+
+        }
+        carregar()
+    }, []);
+
+
+   
+
     return(
         <>
 
         <h1>Dashboard</h1>
+        {carregando && <p>Carregando...</p>}
+        {erro && <p style={{color: "red"}}>{erro}</p>}
+        {!carregando && <p>{dados.length} transações carregadas</p>}
 
         </>
     )
